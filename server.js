bdb535458a420bf7b2aa8edb7802d3bedcf65b13
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-11 11:42:36 -0700

    adding bread names

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads')
   })
-  
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
-  
+
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);

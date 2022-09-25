f53691ce1ab52459695dd0cb6bd0b414fb399a3d
Bree Hewlett <breevhew102@brees-mbp.mynetworksettings.com>
2022-09-24 22:41:02 -0700

    Models and exports

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
-
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -16,6 +16,10 @@ app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
 
+//Mongoose
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 
 // ROUTES
